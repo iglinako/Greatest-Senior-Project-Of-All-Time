cc270fd303e8eda6f452c1dbea2f23dcf52cb160
connor <marbleconnor@gmail.com>
2017-01-29 19:32:21 -0500

    Review text can now be parsed give an arbitrary page number

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,8 +1,13 @@
+from sys import argv
 import urllib2
 from xml.etree import ElementTree as et
 
-#url_base="steamcommunity.com/app/{appid}/homecontent/&p={pagenum}&browsefilter=toprated&browsefilter=mostrecent&l=english&appHubSubSection=10"
+
 url_base = "https://steamcommunity.com/app/{appid}/homecontent/?userreviewsoffset={offset}&p={pagenum}&browsefilter=mostrecent&appHubSubSection=10"
+
+content_start = '<div class="apphub_CardTextContent">'
+content_end = '<div class="UserReviewCardContent_Footer">'
+
 class ReviewScraper(object):
 
     def __init__(self, gameid, startdate):
@@ -19,12 +24,27 @@ class ReviewScraper(object):
 
 
 def parse_review(text):
-    tree = et.fromstring("<root>" + text + "<\\root>").root
-    output = [element.text for element in tree.findall('div class="apphub_CardTextContent"')]
-    return output
+    output = []
+    start = 0
+    try:
+        while(True):
+            start = text.index(content_start, start+len(content_start)-1)
+            end = text.index(content_end, start)
+            output.append(text[start:text.rindex('</div>', start, end)-10])
+
+    except:
+        pass
     
+    return map(clean_review_text, output)
+    
+
+def clean_review_text(review_text):
+    if '</div>' in review_text:
+        return review_text[review_text.rindex('</div>')+7:].strip()
+    return review_text.strip()
+
 if __name__=='__main__':
-    urlob = urllib2.urlopen(url_base.format(appid=324800, offset=30, pagenum=4))
+    urlob = urllib2.urlopen(url_base.format(appid=324800, offset=(int(argv[1])-1)*10, pagenum=int(argv[1])))
     reviews = parse_review(urlob.read())
     for review in reviews:
         print review
